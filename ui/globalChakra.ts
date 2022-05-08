6988d03e64fd4285016eb6633adbedb2df3357a2
yosukekato165 <padyk165@gmail.com>
2022-05-08 10:49:40 +0900

    reset css の追加

--- ui/globalChakra.ts
+++ ui/globalChakra.ts
@@ -1,11 +1,29 @@
 import { extendTheme } from '@chakra-ui/react'
 
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac'
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      900: '#1a365d',
+      800: '#153e75',
+      700: '#2a69ac'
+    }
+  },
+  styles: {
+    global: {
+      'html, body': {
+        color: '#252525',
+        background: '#fff',
+        lineHeight: '1.4142135623',
+        fontSize: 'base',
+        letterSpacing: '0.08em',
+        textAlign: 'justify',
+        textJustify: 'inter-ideograph',
+        fontFamily:
+          '"Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", "BIZ UDPGothic",Meiryo, sans-serif'
+      },
+      li: {
+        listStyle: 'none'
+      }
+    }
   }
-}
-
-export const theme = extendTheme({ colors })
+})
