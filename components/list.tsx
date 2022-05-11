badf6def4170f21f4db2b9fe9574853fd11b3342
yosukekato165 <padyk165@gmail.com>
2022-05-11 23:09:56 +0900

    Modal の移動

--- components/list.tsx
+++ components/list.tsx
@@ -7,32 +7,12 @@ import { textState } from '../recoil/todo'
 
 type Props = {
   list: string[]
+  openModal: () => void
 }
 
-export const List: FC<Props> = ({ list }) => {
+export const List: FC<Props> = ({ list, openModal }) => {
   const [text, setText] = useRecoilState(textState)
-  const [isModalOpen, setIsModalOpen] = useState(false)
 
-  const escFunction = useCallback(
-    (event: KeyboardEvent) => {
-      if (event.key === 'Escape' && isModalOpen) {
-        setIsModalOpen(false)
-      }
-    },
-    [isModalOpen]
-  )
-
-  useEffect(() => {
-    document.addEventListener('keydown', escFunction)
-  }, [escFunction])
-
-  const openModal = () => {
-    setIsModalOpen(true)
-  }
-
-  const closeModal = () => {
-    setIsModalOpen(false)
-  }
   const removeItemAtIndex = (arr: any[], index: number) => {
     return [...arr.slice(0, index), ...arr.slice(index + 1)]
   }
@@ -59,17 +39,6 @@ export const List: FC<Props> = ({ list }) => {
                 </Box>
               </ButtonGroup>
             </Flex>
-            <Box
-              position="fixed"
-              top="0"
-              left="0"
-              w="100%"
-              h="100%"
-              display={isModalOpen ? 'block' : 'none'}
-              bg="blue"
-              opacity="0.3"
-              onClick={closeModal}
-            />
           </li>
         )
       })}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
-import { Container, Flex } from '@chakra-ui/react'
+import { Box, Container, Flex } from '@chakra-ui/react'
 import { useRecoilState } from 'recoil'
 import { textState } from '../recoil/todo'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useCallback, useEffect, useState } from 'react'
 import { RoundedOutlineButton } from '../components/button'
 import { TextInput } from '../components/textInput'
 import { List } from '../components/list'
@@ -10,6 +10,28 @@ import { List } from '../components/list'
 const Home: NextPage = () => {
   const [text, setText] = useRecoilState(textState)
   const [inputText, setInputText] = useState('')
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const escFunction = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === 'Escape' && isModalOpen) {
+        setIsModalOpen(false)
+      }
+    },
+    [isModalOpen]
+  )
+
+  useEffect(() => {
+    document.addEventListener('keydown', escFunction)
+  }, [escFunction])
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
 
   // TODO 型定義をtype TextInput を使いたい
   const setInputValueToInputText = (e: ChangeEvent<HTMLInputElement>) => {
@@ -22,15 +44,28 @@ const Home: NextPage = () => {
   }
 
   return (
-    <Container w="700px" margin="20px auto">
-      {/*  TODO: 文字列が入っていない時のバリデーションを追加する */}
-      <Flex>
-        <TextInput value={inputText} onChange={setInputValueToInputText} />
-        <RoundedOutlineButton onClick={setInputTextToRecoilState} />
-      </Flex>
-      <br />
-      <List list={text.todos} />
-    </Container>
+    <>
+      <Container w="700px" margin="20px auto">
+        {/*  TODO: 文字列が入っていない時のバリデーションを追加する */}
+        <Flex>
+          <TextInput value={inputText} onChange={setInputValueToInputText} />
+          <RoundedOutlineButton onClick={setInputTextToRecoilState} />
+        </Flex>
+        <br />
+        <List list={text.todos} openModal={openModal} />
+      </Container>
+      <Box
+        position="fixed"
+        top="0"
+        left="0"
+        w="100%"
+        h="100%"
+        display={isModalOpen ? 'block' : 'none'}
+        bg="blue"
+        opacity="0.3"
+        onClick={closeModal}
+      />
+    </>
   )
 }
 
